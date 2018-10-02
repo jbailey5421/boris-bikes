585e863bafcbadded720e4e6799fc62741de0b62
Jordan Bailey <jordanbailey201@gmail.com>
2018-10-02 14:18:41 +0000

    upto challenge 15

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,19 +3,27 @@ describe DockingStation do
     it 'responds to release_bike' do
         expect(subject).to respond_to :release_bike
     end
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
   end
-  
   it { is_expected.to respond_to(:dock).with(1).argument }
   
-    # first, let's update our test for #dock
-  it 'docks something' do
-    bike = Bike.new
-    subject.dock(bike)
-    # Again, we need to return the bike we just docked
-    expect(subject.bike).to eq bike
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
-  
-end 
+    describe '#dock' do
+    it 'raises an error when full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+end
